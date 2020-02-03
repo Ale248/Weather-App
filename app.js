b463ee1ab519feb00acb3b2f11420d89a1fc9e18
Alejandro Akifarry <alejandro291097@yahoo.com>
2020-02-03 10:18:38 -0800

    use proxy to get data from api

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 window.addEventListener('load', () => {
-  let long;
-  let lat;
+    let long;
+    let lat;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      long = position.coords
-    })
-  } else {
-    h1.textContent = "not workinggggggg";
-  }
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/c27152790c67a785a74c6618e4d4b067/${lat},${long}`;
+
+            // Fetch data, and THEN... ("then" to wait for the fetch to complete first)
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                });
+        });
+
+
+
+    } else {
+        h1.textContent = "not workinggggggg";
+    }
 });
-
-// halo zulul
