4d2e33feb887b7bf944b7893067dcbbd3139a997
Alejandro Akifarry <alejandro291097@yahoo.com>
2020-02-03 17:06:04 -0800

    display icon and able to change units

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ window.addEventListener('load', () => {
     let lat;
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
+    let temperatureSection = document.querySelector('.temperature-section');
+    let temperatureSpan = document.querySelector('.temperature-section span');
     let locationTimezone = document.querySelector('.location-timezone');
+    
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -20,19 +23,47 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     // same like data.currently.temperature
-                    const {temperature, summary} = data.currently;
+                    let {temperature, summary, icon} = data.currently;
+
+                    temperature = Math.floor(temperature);
 
                     // Set DOM Elements from the API
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
 
+                    // Celcius formula
+                    let celcius = (temperature - 32) * (5 / 9);
+                    
+                    // Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+
+                    // TODO: MAKE SEPARATE FUNCTION
+                    // Change temp to celcius/fahrenheit
+                    // listen to 'click' event
+                    temperatureSection.addEventListener('click', () => {
+                        // === forces type
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celcius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    });
+
                 });
         });
-
-
-
     } else {
         h1.textContent = "not workinggggggg";
+    }
+
+    // skycons.add("icon1", Skycons.PARTLY_CLOUDY_DAY);
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({color: "white"});
+        // replace - with _ because data from api is ex. "partly-cloudy"
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
